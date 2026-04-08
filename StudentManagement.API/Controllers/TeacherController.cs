d696abc434432faaa426b1335f54e94be8fd8336
Ritesh Chakravarti <ritesh.chakravarti@gmail.com>
2026-04-08 23:08:42 +0530

    Added Teacher API Controller
    Add TeacherController with GET endpoint for teachers list
    
    Introduced TeacherController to the API, providing a GET endpoint at /api/teacher that returns a hardcoded list of two teachers with their Id, Name, and Subject. This serves as a basic example for teacher data retrieval.

---
+++ StudentManagement.API/Controllers/TeacherController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new[]
+            {
+                new { Id = 1, Name = "Teacher 1", Subject = "Math" },
+                new { Id = 2, Name = "Teacher 2", Subject = "Science" }
+            });
+        }
+    }
+}

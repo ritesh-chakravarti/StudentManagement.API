2043f0de44bbce8c7b0e390dcd9ed2ba30396958
Ritesh Chakravarti <ritesh.chakravarti@gmail.com>
2026-04-08 23:13:26 +0530

    Fix student not found + add logging

--- StudentManagement.API/Controllers/StudentsController.cs
+++ StudentManagement.API/Controllers/StudentsController.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StudentManagement.API.Models;
 using StudentManagement.API.Services;
+
 namespace StudentManagement.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class StudentsController : ControllerBase
     {
+        private readonly ILogger<StudentsController> _logger;
         private readonly IStudentService _studentService;
 
-        public StudentsController(IStudentService studentService)
+        public StudentsController(IStudentService studentService, Logger<StudentsController> logger)
         {
+            _logger = logger;
             _studentService = studentService;
         }
 
@@ -24,10 +27,13 @@ namespace StudentManagement.API.Controllers
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
+            _logger.LogInformation("Fetching student with Id {Id}", id);
             var student = _studentService.GetById(id);
 
             if (student == null)
             {
+
+                _logger.LogWarning("Student with Id {Id} not found", id);
                 return NotFound($"Student with Id {id} not found.");
             }
 

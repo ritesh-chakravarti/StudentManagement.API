b07df706aa490cf8733e31377bacbad14f9a1890
Ritesh Chakravarti <ritesh.chakravarti@gmail.com>
2026-04-08 23:34:10 +0530

    Added xUnit test project for CI pipeline

---
+++ StudentManagement.API.Tests/StudentServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using StudentManagement.API.Services;
+
+namespace StudentManagement.API.Tests
+{
+    public class StudentServiceTests
+    {
+        [Fact]
+        public void GetAll_ShouldReturnSeededStudents()
+        {
+            // Arrange
+            var service = new StudentService();
+
+            // Act
+            var students = service.GetAll();
+
+            // Assert
+            Assert.NotNull(students);
+            Assert.NotEmpty(students);
+            Assert.True(students.Count >= 2);
+        }
+    }
+}
